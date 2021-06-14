9ff10262ea40fad3fc7f81554762f0b0ced1524f
Misha Bireta <bewoler.del@gmail.com>
2021-06-14 17:04:26 +0300

    added routing

--- registration_app/src/App.jsx
+++ registration_app/src/App.jsx
@@ -1,14 +1,34 @@
-import './front/styles/app.css';
-import LoginForm from './front/components/LoginForm';
-// import UsersTable from './front/components/UsersTable';
+import React from 'react';
+import "./front/styles/app.css";
+import LoginForm from "./front/components/LoginForm";
+import UsersTable from "./front/components/UsersTable";
 
-function App() {
-  return (
-    <div className="app">
-      {/* <UsersTable /> */}
-      <LoginForm />
-    </div>
-  );
+import { Route, Switch, Redirect } from "react-router-dom";
+
+export default function App(props) {
+  const { history } = props;
+
+  function checkPage() {
+    if (localStorage.length > 0 && localStorage.key(0) === "token") {
+      return (
+        <div>
+          <Route history={history} path="/table" component={UsersTable} />
+          <Redirect from="/" to="/table" />
+        </div>
+      );
+    } else if (localStorage <= 0 || localStorage.key(0) !== "token") {
+      return (
+        <div>
+          <Route history={history} path="/" component={LoginForm} />
+          <Redirect from="/table" to="/" />
+        </div>
+      );
+    }
+  };
+
+  return <div className="app">
+    <Switch>
+      <>{checkPage()}</>
+      </Switch>
+      </div>;
 }
-
-export default App;
